c5ababf831e5b67a47eb47323710c0d7c815bc83
renato matos <renato.matos79@gmail.com>
2020-01-13 22:42:44 +0000

    atualizando script mongo02

--- mongo02/print-array.js
+++ mongo02/print-array.js
@@ -1,7 +1,7 @@
 // load("C:/Temp/curso-nodejs/mongo02/print-array.js")
 
 conn = new Mongo();
-db = conn.getDB("test");
+db = conn.getDB("aula");
 var myCursor = db.orders.find( { cust_id: "abc1" } ).forEach( function(row) { 
-	print( "cust_id: " + row.ord_date ); 
+	print( "ord_date: " + row.ord_date ); 
 } );
--- mongo02/print.js
+++ mongo02/print.js
@@ -1,7 +1,7 @@
 // load("C:/Temp/curso-nodejs/mongo02/print.js")
 
 conn = new Mongo();
-db = conn.getDB("test");
+db = conn.getDB("aula");
 var myCursor = db.orders.find( { cust_id: "abc1" } );
 while (myCursor.hasNext()) {
    print(tojson(myCursor.next()));
